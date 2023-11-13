4cfd859b5088bfe024bf7b07577b80849b6d98a4
KTH99 <51234470+KTH99@users.noreply.github.com>
2023-11-13 16:04:02 +0900

    Create preprocessing.py

---
+++ preprocessing.py
@@ -0,0 +1,255 @@
+!pip install mxnet
+!pip install gluonnlp==0.8.0
+!pip install tqdm pandas
+!pip install torch
+!pip install sentencepiece
+!pip install transformers
+!pip install 'git+https://github.com/SKTBrain/KoBERT.git#egg=kobert_tokenizer&subdirectory=kobert_hf'
+from kobert_tokenizer import KoBERTTokenizer
+from transformers import BertModel
+from transformers import AdamW
+from transformers.optimization import get_cosine_schedule_with_warmup
+import torch
+from torch import nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+import gluonnlp as nlp
+import numpy as np
+from tqdm.notebook import tqdm
+import pandas as pd
+import gluonnlp as nlp
+!pip install gluonnlp
+!pip install tokenizers
+
+tokenizer = KoBERTTokenizer.from_pretrained('skt/kobert-base-v1')
+bertmodel = BertModel.from_pretrained('skt/kobert-base-v1', return_dict=False)
+vocab = nlp.vocab.BERTVocab.from_sentencepiece(tokenizer.vocab_file, padding_token='[PAD]')
+
+class BERTDataset(Dataset):
+    def __init__(self, dataset, sent_idx, label_idx, bert_tokenizer, vocab, max_len,
+                 pad, pair):
+        transform = BERTSentenceTransform2(
+            bert_tokenizer, max_seq_length=max_len, vocab=vocab, pad=pad, pair=pair)
+
+        self.sentences = [transform([i[sent_idx]]) for i in dataset]
+        self.labels = [np.int32(i[label_idx]) for i in dataset]
+
+    def __getitem__(self, i):
+        return (self.sentences[i] + (self.labels[i], ))
+
+    def __len__(self):
+        return (len(self.labels))
+
+class BERTSentenceTransform2:
+    r"""BERT style data transformation.
+
+    Parameters
+    ----------
+    tokenizer : BERTTokenizer.
+        Tokenizer for the sentences.
+    max_seq_length : int.
+        Maximum sequence length of the sentences.
+    vocab : Vocab
+        The vocabulary which has cls_token and sep_token registered.
+        If vocab.cls_token is not present, vocab.bos_token is used instead.
+        If vocab.sep_token is not present, vocab.eos_token is used instead.
+    pad : bool, default True
+        Whether to pad the sentences to maximum length.
+    pair : bool, default True
+        Whether to transform sentences or sentence pairs.
+    """
+
+    def __init__(self, tokenizer, max_seq_length, vocab=None, pad=True, pair=True):
+        self._tokenizer = tokenizer
+        self._max_seq_length = max_seq_length
+        self._pad = pad
+        self._pair = pair
+        self._vocab = vocab
+        # RoBERTa does not register CLS token and SEP token
+        if hasattr(self._vocab, 'cls_token'):
+            self._cls_token = self._vocab.cls_token
+        else:
+            self._cls_token = self._vocab.bos_token
+        if hasattr(self._vocab, 'sep_token'):
+            self._sep_token = self._vocab.sep_token
+        else:
+            self._sep_token = self._vocab.eos_token
+        self._padding_token = self._vocab.padding_token
+
+    def __call__(self, line):
+        """Perform transformation for sequence pairs or single sequences.
+
+        The transformation is processed in the following steps:
+        - tokenize the input sequences
+        - insert [CLS], [SEP] as necessary
+        - generate type ids to indicate whether a token belongs to the first
+        sequence or the second sequence.
+        - generate valid length
+
+        For sequence pairs, the input is a tuple of 2 strings:
+        text_a, text_b.
+
+        Inputs:
+            text_a: 'is this jacksonville ?'
+            text_b: 'no it is not'
+        Tokenization:
+            text_a: 'is this jack ##son ##ville ?'
+            text_b: 'no it is not .'
+        Processed:
+            tokens: '[CLS] is this jack ##son ##ville ? [SEP] no it is not . [SEP]'
+            type_ids: 0     0  0    0    0     0       0 0     1  1  1  1   1 1
+            valid_length: 14
+
+        For single sequences, the input is a tuple of single string:
+        text_a.
+
+        Inputs:
+            text_a: 'the dog is hairy .'
+        Tokenization:
+            text_a: 'the dog is hairy .'
+        Processed:
+            text_a: '[CLS] the dog is hairy . [SEP]'
+            type_ids: 0     0   0   0  0     0 0
+            valid_length: 7
+
+        If vocab.cls_token and vocab.sep_token are not present,
+        vocab.bos_token and vocab.eos_token are used instead.
+
+        Parameters
+        ----------
+        line: tuple of str
+            Input strings. For sequence pairs, the input is a tuple of 2 strings:
+            (text_a, text_b). For single sequences, the input is a tuple of single
+            string: (text_a,).
+
+        Returns
+        -------
+        np.array: input token ids in 'int32', shape (batch_size, seq_length)
+        np.array: valid length in 'int32', shape (batch_size,)
+        np.array: input token type ids in 'int32', shape (batch_size, seq_length)
+
+        """
+
+        # convert to unicode
+        text_a = line[0]
+        if self._pair:
+            assert len(line) == 2
+            text_b = line[1]
+
+        tokens_a = self._tokenizer(text_a)
+        tokens_b = None
+
+        if self._pair:
+            tokens_b = self._tokenizer(text_b)
+
+        if tokens_b:
+            # Modifies `tokens_a` and `tokens_b` in place so that the total
+            # length is less than the specified length.
+            # Account for [CLS], [SEP], [SEP] with "- 3"
+            self._truncate_seq_pair(tokens_a, tokens_b,
+                                    self._max_seq_length - 3)
+        else:
+            # Account for [CLS] and [SEP] with "- 2"
+            if len(tokens_a) > self._max_seq_length - 2:
+                tokens_a = tokens_a[0:(self._max_seq_length - 2)]
+
+        # The embedding vectors for `type=0` and `type=1` were learned during
+        # pre-training and are added to the wordpiece embedding vector
+        # (and position vector). This is not *strictly* necessary since
+        # the [SEP] token unambiguously separates the sequences, but it makes
+        # it easier for the model to learn the concept of sequences.
+
+        # For classification tasks, the first vector (corresponding to [CLS]) is
+        # used as as the "sentence vector". Note that this only makes sense because
+        # the entire model is fine-tuned.
+        tokens = []
+        tokens.append(self._cls_token)
+        tokens.extend(tokens_a)
+        tokens.append(self._sep_token)
+        segment_ids = [0] * len(tokens)
+
+        if tokens_b:
+            tokens.extend(tokens_b)
+            tokens.append(self._sep_token)
+            segment_ids.extend([1] * (len(tokens) - len(segment_ids)))
+
+        input_ids = self._vocab[tokens]
+
+        # The valid length of sentences. Only real  tokens are attended to.
+        valid_length = len(input_ids)
+
+        if self._pad:
+            # Zero-pad up to the sequence length.
+            padding_length = self._max_seq_length - valid_length
+            # use padding tokens for the rest
+            input_ids.extend([self._vocab[self._padding_token]] * padding_length)
+            segment_ids.extend([0] * padding_length)
+
+        return np.array(input_ids, dtype='int32'), np.array(valid_length, dtype='int32'),\
+            np.array(segment_ids, dtype='int32')
+
+
+    def _truncate_seq_pair(self, tokens_a, tokens_b, max_length):
+        """Truncates a sequence pair in place to the maximum length."""
+        # This is a simple heuristic which will always truncate the longer sequence
+        # one token at a time. This makes more sense than truncating an equal percent
+        # of tokens from each, since if one sequence is very short then each token
+        # that's truncated likely contains more information than a longer sequence.
+        while True:
+            total_length = len(tokens_a) + len(tokens_b)
+            if total_length <= max_length:
+                break
+            if len(tokens_a) > len(tokens_b):
+                tokens_a.pop()
+            else:
+                tokens_b.pop()
+
+def predict(predict_sentence):
+
+    data = [predict_sentence, '0']
+    dataset_another = [data]
+
+    another_test = BERTDataset(dataset_another, 0, 1, tok, vocab, max_len, True, False)
+    test_dataloader = torch.utils.data.DataLoader(another_test, batch_size=batch_size, num_workers=5)
+
+    model.eval()
+
+    for batch_id, (token_ids, valid_length, segment_ids, label) in enumerate(test_dataloader):
+        token_ids = token_ids.long().to(device)
+        segment_ids = segment_ids.long().to(device)
+
+        valid_length= valid_length
+        label = label.long().to(device)
+
+        out = model(token_ids, valid_length, segment_ids)
+
+        prob = torch.round(F.softmax(out, dim=1), decimals=3)
+        prob = prob * 100
+        print("공포: ", round(float(prob[0][0]), 1), "%, 놀람: ", round(float(prob[0][1]), 1), "%, 분노: ", round(float(prob[0][2]), 1),
+              "%, 슬픔: ", round(float(prob[0][3]), 1), "%, 중립: ", round(float(prob[0][4]), 1), "%, 행복: ", round(float(prob[0][5]), 1),
+              "%, 혐오: ", round(float(prob[0][6]), 1), "%")
+        test_eval=[]
+        for i in out:
+            logits=i
+            logits = logits.detach().cpu().numpy()
+
+            if np.argmax(logits) == 0:
+                test_eval.append("공포가")
+            elif np.argmax(logits) == 1:
+                test_eval.append("놀람이")
+            elif np.argmax(logits) == 2:
+                test_eval.append("분노가")
+            elif np.argmax(logits) == 3:
+                test_eval.append("슬픔이")
+            elif np.argmax(logits) == 4:
+                test_eval.append("중립이")
+            elif np.argmax(logits) == 5:
+                test_eval.append("행복이")
+            elif np.argmax(logits) == 6:
+                test_eval.append("혐오가")
+
+        print(test_eval[0] + " 느껴집니다.")
+
+sentence = ''
+predict(sentence)
